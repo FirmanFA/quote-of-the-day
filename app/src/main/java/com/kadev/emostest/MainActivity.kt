f2cd684a8ebbe467e0ebc5a6e7a8082cb81a5921
firman <fafirmansyah20@gmail.com>
2026-04-24 20:02:20 +0700

    Refactor MainActivity to simplify UI setup and remove unused imports
    * Remove redundant navigation and screen imports.
    * Remove manual status bar light appearance configuration in `onCreate`.
    * Clean up `MainActivity` by delegating navigation logic entirely to `QuoteNavigation`.

--- app/src/main/java/com/kadev/emostest/MainActivity.kt
+++ app/src/main/java/com/kadev/emostest/MainActivity.kt
@@ -4,21 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.core.view.WindowCompat
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.kadev.emostest.ui.navigation.HomeRoute
 import com.kadev.emostest.ui.QuoteNavigation
-import com.kadev.emostest.ui.screen.home.HomeScreen
 import com.kadev.emostest.ui.theme.EMOSTESTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        WindowCompat.getInsetsController(window, window.decorView)?.isAppearanceLightStatusBars = true
         setContent {
             EMOSTESTTheme {
                 QuoteNavigation()
